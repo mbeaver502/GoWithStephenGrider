daf5c0d6b074d53041880677c911e0d95d58b81c
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 18:09:14 -0500

    Replace Sync with go-routine
    Use a go-routine instead of synchronous call.

--- channels/main.go
+++ channels/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
-// Note: This is checking each link synchronously!
-// Each link is checked in turn, waiting for the previous to finish before starting the next.
 func main() {
 	links := []string{
 		"https://google.com",
@@ -17,9 +14,39 @@ func main() {
 		"https://example.com",
 	}
 
-	checkLinksSync(links)
+	// Note: This is checking each link synchronously!
+	// Each link is checked in turn, waiting for the previous to finish before starting the next.
+	// checkLinksSync(links)
+
+	for _, link := range links {
+		// Launch a new go-routine for checkLink().
+		//
+		// When we launch multiple go-routines, the Go scheduler handles them.
+		// By default, Go attempts to use only one CPU core. We can change that so Go uses multiple CPU cores.
+		// If we have one CPU core, then the Go Scheduler runs *one* routine until it finishes or makes a blocking call (like http.Get()).
+		// If Go is working on multiple CPU cores, then the Go Scheduler will run one thread per each "logical" core.
+		//
+		// Concurrency is not parallelism!
+		// Concurrency -- We can have multiple threads executing code. If one thread blocks, another one is picked up and worked on.
+		// Parallelism -- Multiple threads executed at the *exact same time*. This requires multiple CPU cores.
+		//
+		go checkLink(link)
+	}
 }
 
+func checkLink(link string) {
+	// http.Get() is a blocking call, so our routine will be forced to wait until it completes.
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
+
+/*
 func checkLinksSync(links []string) {
 	for _, link := range links {
 		err := checkLinkSync(link)
@@ -45,3 +72,4 @@ func checkLinkSync(link string) error {
 		return errors.New("Link might be down! " + link)
 	}
 }
+*/
