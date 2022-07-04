356a8d71baaad1a33d4a19f19515776525718b60
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 12:53:10 -0500

    Alternate struct Var Declaration
    Declaring a struct using var and assigning fields manually.

--- structs/main.go
+++ structs/main.go
@@ -8,7 +8,15 @@ type person struct {
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	// Create a person variable named alex.
+	// Since we have not assigned initial values, the person fields are initialized to their zero values (empt string).
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
 
 	fmt.Println(alex)
+	fmt.Printf("%+v \n", alex)
 }
