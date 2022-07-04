bca3781d89d43701929123bd95fd08d589b8a340
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 10:52:22 -0500

    Add TestNewDeck Function
    Add function to test results of the newDeck function.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,3 +1,21 @@
 // The xxx_test.go filename tells us that this code exists specifically to test the xxx code.
 // We can run `go test` to run tests.
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be an Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Diamonds" {
+		t.Errorf("Expected last card to be a King of Diamonds, but got %v", d[len(d)-1])
+	}
+}
