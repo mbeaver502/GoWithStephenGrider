32a06f0183b2980db89c05de46b609145a567bd3
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 11:53:56 -0500

    Add Tests for deck Module
    Add tests for the module-based deck.

---
+++ cards_mod/deck/deck_test.go
@@ -0,0 +1,39 @@
+package deck
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := NewDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be an Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Diamonds" {
+		t.Errorf("Expected last card to be a King of Diamonds, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	filename := "_decktesting"
+
+	os.Remove(filename)
+
+	deck := NewDeck()
+	deck.SaveToFile(filename)
+
+	loadedDeck := NewDeckFromFile(filename)
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(deck))
+	}
+
+	os.Remove(filename)
+}
