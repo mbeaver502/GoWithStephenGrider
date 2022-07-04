7bb805b4badfb3aac4983b187211e070d6e5665a
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 12:57:55 -0500

    Alternative struct Declaration
    Declaring and assigning values to a struct with embedded struct.

--- structs/main.go
+++ structs/main.go
@@ -18,13 +18,21 @@ func main() {
 
 	// Create a person variable named alex.
 	// Since we have not assigned initial values, the person fields are initialized to their zero values (empt string).
-	var alex person
+	//var alex person
+	//alex.firstName = "Alex"
+	//alex.lastName = "Anderson"
+	//alex.contact.email = "john@example.com"
+	//alex.contact.zip = "20007"
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	alex.contact.email = "john@example.com"
-	alex.contact.zip = "20007"
+	john := person{
+		firstName: "John",
+		lastName:  "Doe",
+		contact: contactInfo{
+			email: "john@example.com",
+			zip:   "12345",
+		},
+	}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v \n", alex)
+	fmt.Println(john)
+	fmt.Printf("%+v \n", john)
 }
