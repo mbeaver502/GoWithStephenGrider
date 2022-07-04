0e1d8dd07b62150f8a4f4ba70649e20f5080a2d1
Michael Beaver <jmbeaver1@gmail.com>
2022-07-03 20:21:57 -0500

    Tabs to Spaces

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,20 +1,20 @@
 /*
-	We can execute some commands from terminal:
+   We can execute some commands from terminal:
 
-		go run <filenames>		Build *and* execute one or more Go files
-		go build <filenames> 	Build one or more Go files
+       go run <filenames>		Build *and* execute one or more Go files
+       go build <filenames> 	Build one or more Go files
 
 */
 
 // In Go, a package is like a project or workspace or namespace
 // A package can have many related Go files inside it -- but each must declare the same package name
 // Two types of packages:
-//		(1) Executable -- that which generates an executable file
-//		(2) Reusable -- that which can be re-used as "helper" methods (e.g., code libraries)
+//      (1) Executable -- that which generates an executable file
+//      (2) Reusable -- that which can be re-used as "helper" methods (e.g., code libraries)
 // How do we know if a package is executable or reusable? The name of the package determines this!
-// 		The package name of "main" tells the Go compiler that this is an executable package!
-//		An executable package *must* have a func main!
-// 		Any other name will tell Go that this is *not* an executable package!
+//      The package name of "main" tells the Go compiler that this is an executable package!
+//      An executable package *must* have a func main!
+//      Any other name will tell Go that this is *not* an executable package!
 package main
 
 // We can tell the Go compiler that our program depends on other packages.
