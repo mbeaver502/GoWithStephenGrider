48011cbf8a3bd909a463a17c14626a5a927c25f6
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 15:34:23 -0500

    Naive Implementation
    Naive implementation for two types with similar functionality
    but different implementation details.
    Will not compile because we are trying to overload printGreeting().
    Good candidate for interfaces.

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+// Empty structs for the sake of example.
+// Imagine these structs have similar functionality but different implementation details.
+type englishBot struct{}
+type spanishBot struct{}
+
+func (eb englishBot) getGreeting() string {
+	// Some complicated logic that's different to the spanishBot version of getGreeting...
+	return "Hello! :)"
+}
+
+func (sb spanishBot) getGreeting() string {
+	// Some complicated logic that's different to the englishBot version of getGreeting...
+	return "Hola! :)"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
 }
