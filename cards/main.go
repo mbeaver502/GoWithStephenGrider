0a129ee16e1156527b71931ac3e7241c57812685
Michael Beaver <jmbeaver1@gmail.com>
2022-07-03 20:52:56 -0500

    Add newCard Function
    Add newCard function to replace hardcoded value in func main.

--- cards/main.go
+++ cards/main.go
@@ -16,8 +16,15 @@ func main() {
 	//      var card string = "Ace of Spades"
 	//
 	// Alternatively, we can let the Go compiler infer the strong type is a string:
-	card := "Ace of Spades"   // := is only for assigning a value to a newly created variable!
-	card = "Five of Diamonds" // Notice variable assignment does not use := for extant variables!
+	// 		card := "Ace of Spades"   	:= is only for assigning a value to a newly created variable!
+	// 		card = "Five of Diamonds" 	Notice variable assignment does not use := for extant variables!
+
+	card := newCard() // Even now the Go compiler can infer that newCard returns string, so card is string.
 
 	fmt.Println(card)
+}
+
+// The newCard function has a return type of string. This function *must* return a string.
+func newCard() string {
+	return "Five of Diamonds"
 }
