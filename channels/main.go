f6d0112362c9d6a5bccb4a2aa01c85f4c08b39cb
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 17:42:42 -0500

    Naive Synchronous Version
    First pass version using a naive, synchronous routine.
    Each link is checked in turn, waiting for the previous to finish.

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
+// Note: This is checking each link synchronously!
+// Each link is checked in turn, waiting for the previous to finish before starting the next.
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://example.com",
+	}
+
+	for _, link := range links {
+		err := checkLink(link)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Link appears up! %v\n", link)
+		}
+	}
+}
+
+func checkLink(link string) error {
+	resp, err := http.Get(link)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == 200 {
+		return nil
+	} else {
+		return errors.New("Link might be down! " + link)
+	}
 }
