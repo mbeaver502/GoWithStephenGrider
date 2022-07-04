37b99071e07f28d8a11b3882d4cbd5058d926bb4
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 15:08:16 -0500

    Add map Examples
    Creating maps and iterating over key-value pairs.

--- maps/main.go
+++ maps/main.go
@@ -5,6 +5,36 @@
 //
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//colors := map[string]string{
+	//	"red":   "#FF0000",
+	//	"black": "#000000",
+	//	"white": "#FFFFFF",
+	//}
+
+	// The zero-value of an unassigned map is an empty map [].
+	// var colors map[string]string
+
+	// We can use the make() function to allocate memory and initialize a map.
+	colors := make(map[string]string)
+
+	// We can use [] syntax to manually access key-value pairs.
+	colors["red"] = "#FF0000"
+	colors["white"] = "#FFFFFF"
+	colors["black"] = "#000000"
+
+	fmt.Println(colors)
+
+	// We can delete key-value pairs from the map.
+	// delete(colors, "white")
+
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for key, value := range m {
+		fmt.Println("Hex code for", key, "is", value)
+	}
 }
