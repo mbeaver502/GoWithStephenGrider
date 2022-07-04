572b44b460b7d34c550ee549bd119dcc37d69d70
Michael Beaver <jmbeaver1@gmail.com>
2022-07-04 14:54:17 -0500

    Add maps Project
    Add project for exploring the map data structure.

---
+++ maps/main.go
@@ -0,0 +1,10 @@
+// A map is a data structure that maps a key-value pair, similar to a dict in Python or C#.
+// The keys are statically typed, and all keys must be the same type.
+// The values are statically typed, and all values must be the same type.
+// The keys and values do not need to be the same type (e.g., an int could be a key for string value).
+//
+package main
+
+func main() {
+
+}
