75541a085f7cb1d9fce5ac2ff97ab5e40acef20d
Michael Beaver <jmbeaver1@gmail.com>
2022-07-03 20:10:42 -0500

    Add package Explanations
    Add some code comments explaining how the package keyword
    works for identifying executable code.

--- helloworld/main.go
+++ helloworld/main.go
@@ -5,14 +5,22 @@
 		go build <filenames> 	Build one or more Go files
 
 */
+
+// In Go, a package is like a project or workspace or namespace
+// A package can have many related Go files inside it -- but each must declare the same package name
+// Two types of packages:
+//		(1) Executable -- that which generates an executable file
+//		(2) Reusable -- that which can be re-used as "helper" methods (e.g., code libraries)
+// How do we know if a package is executable or reusable? The name of the package determines this!
+// 		The package name of "main" tells the Go compiler that this is an executable package!
+//		An executable package *must* have a func main!
+// 		Any other name will tell Go that this is *not* an executable package!
 package main
 
-// the fmt package gives us access to std I/O operations like Println
+// The fmt package gives us access to std I/O operations like Println
 import "fmt"
 
-/*
-	This is a multi-line comment.
-*/
+// Since this is an executable package (package main), we *must* have a func main entrypoint
 func main() {
 	fmt.Println("hello world :)")
 }
